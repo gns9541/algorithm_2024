382c8b4a2b32e512ddcea063b576c6f5bc9e6c08
서동훈 <gns9541gns@naver.com>
2024-02-29 14:29:35 +0900

    2024/02/29 백준 알고리즘

--- 올림피아드/8982.py
+++ 올림피아드/8982.py
@@ -3,19 +3,23 @@ Point = [list(map(int, input().split())) for _ in range(N)]
 K = int(input())
 Hole = [list(map(int, input().split())) for _ in range(K)]
 w = Point[-1][0]
-print(Point)
-print(w)
 h = 0
 for i in range(N):
     h = max(h,Point[i][1])
-print(h)
-print(Hole)
+
+
+
 arr = [[0]*(w+1) for _ in range(h+1)]
 print(*arr,sep="\n")
 
-for i in range(h+1):
-    for j in range(w+1):
-        pass
+# for i in range(N):
+#     if Point[i][0] == Point[i+1][0]:
+#         for a in range()
+
+
+
+
+        
 
 
 
